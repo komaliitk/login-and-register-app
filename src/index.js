cc26d7dbd9e324d0780f9942c7a34c10c59cddb4
komaliitk <komalkanaujia8@gmail.com>
2022-04-04 19:39:38 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// import Login from './components/login/login';
+import Homepage from './components/homepage/homepage';
+import Register from './components/register/register';
+import Login from './components/login/login';
+import { useState } from 'react';
+
+const [ user, setLoginUser] = useState({})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    
+    <Router>
+      <Routes>
+        
+        <Route exact path="/"  element={<Homepage />} />
+        <Route path="/login"  element={<Login />} />
+        
+        <Route path="/register"  element={<Register />} />
+        
+      
+      </Routes>
+    </Router>
+    
+   
+   
+   
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +37,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
